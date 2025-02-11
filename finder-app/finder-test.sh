e1a9e863dcbdbd0bea0b657a7ccf04128bbf1ac6
TorikTheGreat <ocramsalazar@gmail.com>
2025-02-11 12:35:36 -0600

    Modified finder-test.sh as per assignment 4 part 2 instructions

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -8,7 +8,13 @@ set -u
 NUMFILES=10
 WRITESTR=AELD_IS_FUN
 WRITEDIR=/tmp/aeld-data
-username=$(cat conf/username.txt)
+username=$(cat /etc/finder-app/conf/username.txt)
+
+# Assignment 4 part 2 changes: given that the path to this script is
+# /path/to/script/finder-test.sh, it must be assumed that all executables
+# will be in this path and all config files will fe at /etc/finder-app/conf
+
+ORIG_PATH=$( dirname $0 )
 
 if [ $# -lt 3 ]
 then
@@ -32,7 +38,7 @@ echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}"
 rm -rf "${WRITEDIR}"
 
 # create $WRITEDIR if not assignment1
-assignment=`cat ../conf/assignment.txt`
+assignment=`cat etc/finder-app/conf/assignment.txt`
 
 if [ $assignment != 'assignment1' ]
 then
@@ -54,10 +60,13 @@ fi
 
 for i in $( seq 1 $NUMFILES)
 do
-	./writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
+	./"${ORIG_PATH}"/writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
 
-OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")
+OUTPUTSTRING=$(./"${ORIG_PATH}"/finder.sh "$WRITEDIR" "$WRITESTR")
+
+# Assignment 4 part 2 addition: write output of finder.sh to /tmp/assignment4-result.txt
+echo "${OUTPUTSTRING}" > /tmp/assignment4-result.txt
 
 # remove temporary directories
 rm -rf /tmp/aeld-data
